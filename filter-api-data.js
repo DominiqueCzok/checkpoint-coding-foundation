4f933da14d7c4cfccf82b2ff6cd567953941e47a
Dominique Czok <d.czok@web.de>
2022-04-27 22:14:51 +0200

    initial commit

--- filter-api-data.js
+++ filter-api-data.js
@@ -1,3 +1,23 @@
-function filterApiData(apiData, mandatoryKeys) {
-  // Add you solution here
+function filterApiData(apiData, mandatoryKeys) {    
+  let result = [];
+  
+  for (let i = 0; i < apiData.length; i++) {
+    const obj = apiData[i];
+    let containsAllKeys = true;
+    
+    for (let x = 0; x < mandatoryKeys.length; x++) {      
+      if (Object.keys(obj).includes(mandatoryKeys[x])) {        
+        containsAllKeys = true;
+      } else {
+        containsAllKeys = false;
+        break;
+      }
+    }
+    
+    if (containsAllKeys) {
+      result.push(obj);
+    }
+  }
+  
+  return result;
 }
